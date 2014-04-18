f5ca9d03ae04bdbad8423cff4d6a942d12c68289
Markus Tenghall <matehall@gmail.com>
2014-04-19 00:43:02 +0200

    New fsm.

--- suckomatic3000/suck3.cpp
+++ suckomatic3000/suck3.cpp
@@ -7,82 +7,139 @@ const int MAX_NUMBER_OF_MINUTES = 3;
 
 //The setup function is called once at startup of the sketch
 void setup() {  // initialize serial communication
-// Serial.begin(9600);
+	Serial.begin(9600);
 
 	pinMode(TRIGGER_PIN, OUTPUT);
 	pinMode(VAC_ON_BUTTON_PIN, OUTPUT);
 	pinMode(VAC_OFF_BUTTON_PIN, OUTPUT);
 	pinMode(ECHO_PIN, INPUT);
 	pinMode(LAMP_PIN, OUTPUT);
-	pinMode(photoresistor_pin,INPUT);
+	pinMode(PHOTORESISTOR_PIN, INPUT);
 
 	lcd.begin(lcd_columns, lcd_row);
 }
 
-void loop() {
-	if (error_state) {
-		// Need user intervension.
-	} else {
+static int get_next_event() {
 
-		fuel_level = get_fuel_level();
-		photoResistorValue = analogRead(photoresistor_pin);
+	photoResistorValue = analogRead(PHOTORESISTOR_PIN);
+	fuel_level = get_fuel_level();
 
-		lcd.setCursor(0, 0);
-		lcd.print("R:     ");
-		lcd.setCursor(2, 0);
-		lcd.print(photoResistorValue);
-
-		lcd.setCursor(0, 1);
-		lcd.print("L:      ");
-		lcd.setCursor(2, 1);
-		lcd.print(fuel_level);
-
-		//Serial.print(fuel_level);
-		//Serial.print("fuel_level");
-		//Serial.println();
-
-		lcd.setCursor(8, 0);
-		lcd.print("S1:     ");
-		lcd.setCursor(11, 0);
-
+	switch (state) {
+	case ST_NORMAL:
 		if (box_is_empty()) {
-			lcd.print("Empty");
-			turn_light_on();
-			last_major_state = EMPTY;
-
-		} else if (box_is_full()) {
-			lcd.print("Full");
-			last_major_state = FULL;
-			turn_vac_off();    //Just to be sure.
-			turn_light_off();
-		} else {
-			lcd.print("Normal");
+			return EV_BOX_EMPTY;
+		}
+		return EV_NO_EVENT;
+		break;
+	case ST_CYCLONE_FILLING:
+		if (box_is_full()) {
+			return EV_BOX_FULL;
 		}
 
-		lcd.setCursor(8, 1);
-		lcd.print("Vac:     ");
-		lcd.setCursor(12, 1);
-
-		if (last_major_state == EMPTY) {
-			if (cyclon_is_full(photoResistorValue)) {
-				lcd.print("OFF");
-				turn_vac_off();
-			} else {
-				turn_vac_on();
-				lcd.print("ON");
-			}
+		if (cycone_full()) {
+			return EV_CYCLONE_FULL;
 		}
-		delay(1000);
 
-		//if (vac_is_on() && vac_has_been_on_more_then(MAX_NUMBER_OF_MINUTES)) {
+		return EV_NO_EVENT;
+		break;
+	case ST_CYCLONE_EMPTENING:
+		if (box_is_full()) {
+			return EV_BOX_FULL;
+		}
 
-		//	turn_vac_off();
-		//	error_state = VACONTOLONG;
-			// Need restart;
-			//Send msg?
-		//}
+		if (cyclone_empty()) {
+			return EV_CYCLONE_EMPTY;
+		}
+
+		return EV_NO_EVENT;
+		break;
+	default:
+		return EV_NO_EVENT;
 	}
 }
+
+static int box_empty() {
+	turn_vac_on();
+	return ST_CYCLONE_FILLING;
+}
+
+static int box_full() {
+	turn_vac_off();
+
+	return ST_NORMAL;
+}
+
+static int cycone_full() {
+	turn_vac_off();
+	return ST_CYCLONE_EMPTENING;
+}
+
+static int cyclone_empty() {
+	turn_vac_on();
+	return ST_CYCLONE_FILLING;
+}
+
+//	if (error_state) {
+//		// Need user intervension.
+//	} else {
+//
+//		fuel_level = get_fuel_level();
+//		photoResistorValue = analogRead(photoresistor_pin);
+//
+//		lcd.setCursor(0, 0);
+//		lcd.print("R:     ");
+//		lcd.setCursor(2, 0);
+//		lcd.print(photoResistorValue);
+//
+//		lcd.setCursor(0, 1);
+//		lcd.print("L:      ");
+//		lcd.setCursor(2, 1);
+//		lcd.print(fuel_level);
+//
+//		//Serial.print(fuel_level);
+//		//Serial.print("fuel_level");
+//		//Serial.println();
+//
+//		lcd.setCursor(8, 0);
+//		lcd.print("S1:     ");
+//		lcd.setCursor(11, 0);
+//
+//		if (box_is_empty()) {
+//			lcd.print("Empty");
+//			turn_light_on();
+//			last_major_state = EMPTY;
+//
+//		} else if (box_is_full()) {
+//			lcd.print("Full");
+//			last_major_state = FULL;
+//			turn_vac_off();    //Just to be sure.
+//			turn_light_off();
+//		} else {
+//			lcd.print("Normal");
+//		}
+//
+//		lcd.setCursor(8, 1);
+//		lcd.print("Vac:     ");
+//		lcd.setCursor(12, 1);
+//
+//		if (last_major_state == EMPTY) {
+//			if (cyclon_is_full(photoResistorValue)) {
+//				lcd.print("OFF");
+//				turn_vac_off();
+//			} else {
+//				turn_vac_on();
+//				lcd.print("ON");
+//			}
+//		}
+//		delay(1000);
+
+//if (vac_is_on() && vac_has_been_on_more_then(MAX_NUMBER_OF_MINUTES)) {
+
+//	turn_vac_off();
+//	error_state = VACONTOLONG;
+// Need restart;
+//Send msg?
+//}
 
 boolean vac_has_been_on_more_then(int max_vacc_on_in_minutes) {
 
@@ -119,15 +176,20 @@ long get_fuel_level() {
 }
 
 boolean box_is_empty() {
-	return fuel_level > box_empty_level;
+	return fuel_level > BOX_EMPTY_LEVEL;
 }
 
 boolean box_is_full() {
-	return fuel_level < box_full_level;
+	return fuel_level < BOX_FULL_LEVEL;
 }
 
 boolean cyclon_is_full(int resistor_value) {
-	return resistor_value > CYCLON_FULL;
+	return resistor_value > CYCLONE_FULL;
+}
+
+boolean cyclon_full() {
+
+	return photoResistorValue > CYCLONE_FULL;
 }
 
 void turn_vac_on() {
@@ -157,11 +219,79 @@ boolean vac_is_off() {
 	return vac_state == VAC_OFF;
 }
 
-void turn_light_on() {
-	digitalWrite(LAMP_PIN, HIGH);
+//void turn_light_on() {
+//	digitalWrite(LAMP_PIN, HIGH);
+//}
+//
+//void turn_light_off() {
+//	digitalWrite(LAMP_PIN, LOW);
+//}
+
+static String get_state_string() {
+	switch (state) {
+	case ST_CYCLONE_EMPTENING:
+		return "Cylone Emptening";
+		break;
+	case ST_CYCLONE_FILLING:
+		return "Cylone Filling";
+		break;
+	case ST_NORMAL:
+		return "Normal";
+		break;
+	default:
+		return "Err";
+	}
 }
 
-void turn_light_off() {
-	digitalWrite(LAMP_PIN, LOW);
+void updateLCD() {
+	lcd.setCursor(0, 0);
+	lcd.print("R:     ");
+	lcd.setCursor(2, 0);
+	lcd.print(photoResistorValue);
+
+	lcd.setCursor(7, 0);
+	lcd.print("L:      ");
+	lcd.setCursor(9, 0);
+	lcd.print(fuel_level);
+
+	lcd.setCursor(14, 0);
+	lcd.print("V:    ");
+	lcd.setCursor(15, 0);
+	lcd.print(vac_state);
+
+	lcd.setCursor(0, 1);
+	lcd.print("S:               ");
+	lcd.setCursor(2, 1);
+	lcd.print(get_state_string());
+
+	Serial.print("photoResistorValue:");
+	Serial.print(photoResistorValue);
+	Serial.print(" ");
+	Serial.print("fuel_level:");
+	Serial.print(fuel_level);
+	Serial.print(" ");
+	Serial.print("state:");
+	Serial.print(get_state_string());
+	Serial.println();
+
 }
 
+void loop() {
+
+	int i;
+
+	state = ST_NORMAL;
+	while (state != ST_TERM) {
+		delay(1000);
+		event = get_next_event();
+		for (i = 0; i < TRANS_COUNT; i++) {
+			if ((state == trans[i].state) || (ST_ANY == trans[i].state)) {
+				if ((event == trans[i].event) || (EV_ANY == trans[i].event)) {
+					state = (trans[i].fn)();
+					break;
+				}
+			}
+		}
+		updateLCD();
+	}
+}
--- suckomatic3000/suck3.h
+++ suckomatic3000/suck3.h
@@ -22,50 +22,39 @@ void setup();
 
 //add your function definitions for the project suck3 here
 
-const int box_empty_level = 65;
-const int box_full_level = 45;
+typedef struct {
+	int state;
+	int event;
+	int (*fn)(void);
+} tTransition;
 
-const int ZERO_PIN = 4;
-const int LAMP_PIN = 5;
-const int VAC_ON_BUTTON_PIN = 2;
-const int VAC_OFF_BUTTON_PIN = 3;
-const int ECHO_PIN = 12;
-const int TRIGGER_PIN = 13;
+#define ST_ANY -1
+#define ST_NORMAL  0
+#define ST_CYCLONE_FILLING  1
+#define ST_CYCLONE_EMPTENING 2
+#define ST_TERM 3
 
-long fuel_level = 0;
+int state;
+int event;
 
-//States
-const int EMPTY = 1;
-const int FULL = 0;
-int last_major_state = EMPTY;
-const int CLEAR = 0;
-const int VACONTOLONG = 1;
+#define EV_ANY              -1
+#define EV_CYCLONE_FULL		5000
+#define EV_CYCLONE_EMPTY		5001
+#define EV_BOX_FULL			5002
+#define EV_BOX_EMPTY		5003
+#define EV_NO_EVENT 5004
 
-int error_state = CLEAR;
+//Events
+static int box_empty(void);
+static int cycone_full(void);
+static int cyclone_empty(void);
+static int box_full(void);
+static int self_trans(void){return state;};
 
-//Vac control
-const int VAC_OFF = 0;
-const int VAC_ON = 1;
-const int CYCLON_FULL = 600;
-int vac_state = VAC_OFF;
-
-unsigned long vacc_start_time = 0;
-
-//Cyclon level control
-int photoResistorValue = 0;
-
-const int photoresistor_pin = 1;
-
-const int lcd_rs = 8;
-const int lcd_enable = 9;
-const int lcd_d4 = 4;
-const int lcd_d5 = 5;
-const int lcd_d6 = 6;
-const int lcd_d7 = 7;
-const int lcd_columns = 16;
-const int lcd_row = 2;
-
-LiquidCrystal lcd(lcd_rs, lcd_enable, lcd_d4, lcd_d5, lcd_d6, lcd_d7);
+//Helpers
+static int get_next_event(void);
+static void updateLCD(void);
+static String get_state_string(void);
 
 long get_fuel_level();
 boolean box_is_empty();
@@ -78,6 +67,73 @@ void turn_light_on();
 boolean vac_is_on();
 boolean cyclon_is_full(int);
 boolean vac_has_been_on_more_then(int);
+
+tTransition trans[] = {
+		{ ST_NORMAL, EV_BOX_EMPTY, &box_empty },
+		{ ST_CYCLONE_FILLING, EV_BOX_FULL, &box_full },
+		{ ST_CYCLONE_FILLING, EV_CYCLONE_FULL, &cycone_full },
+		{ ST_CYCLONE_FILLING, EV_NO_EVENT, &self_trans },
+		{ ST_CYCLONE_EMPTENING, EV_CYCLONE_EMPTY, &cyclone_empty },
+		{ ST_CYCLONE_EMPTENING, EV_BOX_EMPTY, &box_full },
+		{ ST_CYCLONE_EMPTENING, EV_NO_EVENT, &self_trans },
+
+};
+
+#define TRANS_COUNT (sizeof(trans)/sizeof(*trans))
+
+
+//Levels
+const int BOX_EMPTY_LEVEL = 65;
+const int BOX_FULL_LEVEL = 45;
+const int CYCLONE_FULL = 600;
+
+//Digital pins
+const int ZERO_PIN = 0;
+const int LAMP_PIN = 1;
+const int VAC_ON_BUTTON_PIN = 2;
+const int VAC_OFF_BUTTON_PIN = 3;
+const int ECHO_PIN = 12;
+const int TRIGGER_PIN = 13;
+
+//Analog pins
+const int PHOTORESISTOR_PIN = 1;
+
+long fuel_level = 0;
+int photoResistorValue = 0;
+
+const int VAC_OFF = 0;
+const int VAC_ON = 1;
+
+//States
+//const int EMPTY = 1;
+//const int FULL = 0;
+//int last_major_state = EMPTY;
+//const int CLEAR = 0;
+//const int VACONTOLONG = 1;
+//
+//int error_state = CLEAR;
+
+//Vac control
+
+int vac_state = VAC_OFF;
+
+unsigned long vacc_start_time = 0;
+
+
+//LCD
+const int lcd_rs = 8;
+const int lcd_enable = 9;
+const int lcd_d4 = 4;
+const int lcd_d5 = 5;
+const int lcd_d6 = 6;
+const int lcd_d7 = 7;
+const int lcd_columns = 16;
+const int lcd_row = 2;
+
+
+LiquidCrystal lcd(lcd_rs, lcd_enable, lcd_d4, lcd_d5, lcd_d6, lcd_d7);
+
+
 
 //Do not add code below this line
 #endif /* _suck3_H_ */
